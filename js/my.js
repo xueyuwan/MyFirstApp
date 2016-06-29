c9b344901a7236a48e27c511c105bae3b27381a6
topshadow <731502029@qq.com>
2016-06-29 16:11:36 +0800

    登录功能Ing

--- js/my.js
+++ js/my.js
@@ -89,10 +89,9 @@ angular.module('ionicApp', ['ionic'])
          popup.show('提示','用户名或密码不能为空');
 
       }else if(!/^(13[0-9]|14[0-9]|15[0-9]|18[0-9])\d{8}$/.test($scope.user.username)){
-        
         popup.show('提示','请输入正确的手机号');
       }else{
-          console.log('登录成功');
+        $state.go('sign-up');
       }
 
 
