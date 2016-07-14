68412d20f30cf246bc3c966d05e778585a90221e
topshadow <731502029@qq.com>
2016-07-15 00:58:14 +0800

    正在做聊天系统

--- www/js/chat/messageDetailCtrl.js
+++ www/js/chat/messageDetailCtrl.js
@@ -1,14 +1,14 @@
 var messages;
 angular.module('chat.controllers')
 .controller('messageDetailCtrl',
-    function($scope, $stateParams, messageService, $ionicScrollDelegate, $timeout) {
+    function($rootScope,$scope, $stateParams, messageService, $ionicScrollDelegate, $timeout) {
             $scope.messages =[];
         var viewScroll = $ionicScrollDelegate.$getByHandle('messageDetailsScroll');
         messages =$scope.messages;
         var socket = io.connect('http://localhost:3000/');
         $scope.sendMessage = function(){
             socket.emit('send message',$scope.send_content);
-            $scope.messages.push({content:$scope.send_content});
+            $scope.messages.push({content:$scope.send_content,pic:$rootScope.user.headpic,isFromeMe:true});
             console.log($scope.messages);
             $scope.send_content="";
         };
